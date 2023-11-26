58e449b0e01b7895d878e4e1b6c58261afcef92a
abdallah <abdallahassal12@gmail.com>
2023-11-26 22:10:57 +0200

    the user will enter the Title and Url

--- index.py
+++ index.py
@@ -4,3 +4,8 @@ import json
 def open_tab():
      title = input("Enter the title of the new tab: ")
      url = input("Enter the URL of the new tab: ")
+# the user will enter the Title and Url 
+     tabs[title] = {"title": title, "url": url} 
+print("Successfully opened tab ")
+
+
